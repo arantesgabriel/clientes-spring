a47c4ddb40634f8b164bde7a88bc4a89d4ff07c4
Gabriel Arantes <gabrielpacontato@gmail.com>
2023-07-06 21:33:46 -0300

    Implementado a classe 'CorsConfiguration' para permitir que o front-end consiga enviar as requisições necessárias.

--- src/main/java/com/example/clientes/controller/ClientesController.java
+++ src/main/java/com/example/clientes/controller/ClientesController.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Stream;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("clientes")
 public class ClientesController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/buscar")
+    @GetMapping("/listar")
     public Stream<ClienteResponseDTO> buscarClientes() {
         Stream<ClienteResponseDTO> clientes = clienteRepository.findAll().stream().map(ClienteResponseDTO::new);
         return clientes;
