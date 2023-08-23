8074ef09e8bea8cfb822cd66b98f28ea3947b71d
Gabriel Arantes <gabrielpacontato@gmail.com>
2023-08-23 00:45:07 -0300

    Inserido o arquivo 'import.sql' com vários inserts para a tabela de clientes.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tcliente (nomcli, email, senha) VALUES ('Rafaela Caroline da Conceição', 'rafaela-daconceicao70@montcalm.com.br', 'HLQL3d9HOX');
+INSERT INTO tcliente (nomcli, email, senha) VALUES ('Andreia Milena Juliana Porto', 'andreia_porto@humanfit.com.br', 'WCQg3PsISI');
+INSERT INTO tcliente (nomcli, email, senha) VALUES ('Luiza Tereza Ramos', 'luiza.tereza.ramos@rogerstenis.com.br', 'TBWyHuORWy');
+INSERT INTO tcliente (nomcli, email, senha) VALUES ('Pedro Erick Aparício', 'pedro.erick.aparicio@prlme.com.br', 'si3AwJmD2K');
+INSERT INTO tcliente (nomcli, email, senha) VALUES ('Gabriela Ana de Paula', 'gabriela-depaula93@way2goidiomas.com.br', 'ymAzmybRRh');
+INSERT INTO tcliente (nomcli, email, senha) VALUES ('Luan Ruan Nicolas Fernandes', 'lorenzosamuelmoraes@boldcron.com.br', 'rWLhBE9Kun');
+INSERT INTO tcliente (nomcli, email, senha) VALUES ('Roberto Davi Nunes', 'roberto_davi_nunes@fosjc.unesp.br', 'jZefRsY0kg');
